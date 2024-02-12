9ba533faec540cea50c9d1033478928165bf2612
Renan Muller <renanmd@gmail.com>
2024-02-12 12:58:11 -0300

    Initial Commit

---
+++ SwiftuiEnvironment/SwiftuiEnvironmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftuiEnvironmentApp.swift
+//  SwiftuiEnvironment
+//
+//  Created by Renan Diniz on 12/02/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftuiEnvironmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
